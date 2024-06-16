d123cae6102726c79077056a845d1ba9d61cb093
Joe Fang <25814618+MinecraftFuns@users.noreply.github.com>
2024-06-16 19:02:01 -0400

    [feat] placeholder for Chrome API

--- bypass-paywalls.user.js
+++ bypass-paywalls.user.js
@@ -183,9 +183,58 @@
 (function () {
     'use strict';
 
+    const placeholderFn = () => { };
+    // Running as a user script, the script needs to be patched for the missing Chrome APIs
     const extensionApi = {
-
-    }
+        "runtime": {
+            "sendMessage": placeholderFn,
+            "openOptionsPage": placeholderFn,
+            "onInstalled": {
+                "addListener": placeholderFn
+            },
+            "lastError": null
+        },
+        "storage": {
+            "sync": {
+                "set": placeholderFn,
+                "get": placeholderFn
+            },
+            "onChanged": {
+                "addListener": placeholderFn
+            }
+        },
+        "tabs": {
+            "onUpdated": {
+                "addListener": placeholderFn
+            },
+            "onActivated": {
+                "addListener": placeholderFn
+            },
+            "get": placeholderFn,
+            "executeScript": placeholderFn
+        },
+        "browserAction": {
+            "setBadgeBackgroundColor": placeholderFn,
+            "setBadgeText": placeholderFn
+        },
+        "webRequest": {
+            "onBeforeRequest": {
+                "addListener": placeholderFn
+            },
+            "onHeadersReceived": {
+                "addListener": placeholderFn
+            },
+            "OnBeforeSendHeadersOptions": placeholderFn,
+            "onBeforeSendHeaders": placeholderFn,
+            "onCompleted": {
+                "addListener": placeholderFn
+            }
+        },
+        "cookies": {
+            "getAll": placeholderFn,
+            "remove": placeholderFn
+        }
+    };
 
     if (!matchDomain(['seekingalpha.com', 'sfchronicle.com', 'cen.acs.org', 'elmundo.es', 'scmp.com', 'nytimes.com'])) {
         window.localStorage.clear();
@@ -1248,9 +1297,9 @@
         // Remove cookies for custom sites
         _removeCookies = removeCookies.concat(items.customSites);
 
-        if (extensionApi === chrome) {
-            initGA();
-        }
+        // if (extensionApi === chrome) {
+        //     initGA();
+        // }
     });
 
     // Listen for changes to options
@@ -1521,22 +1570,22 @@
     });
 
     // Google Analytics to anonymously track DAU (Chrome only)
-    function initGA() {
-        // (function (i, s, o, g, r, a, m) {
-        //     i.GoogleAnalyticsObject = r;
-        //     i[r] = i[r] || function () {
-        //         (i[r].q = i[r].q || []).push(arguments);
-        //     }, i[r].l = 1 * new Date();
-        //     a = s.createElement(o), m = s.getElementsByTagName(o)[0];
-        //     a.async = 1;
-        //     a.src = g;
-        //     m.parentNode.insertBefore(a, m);
-        // })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-        // ga('create', 'UA-69824169-2', 'auto');
-        // ga('set', 'checkProtocolTask', null);
-        // ga('set', 'anonymizeIp', true);
-        // ga('send', 'pageview');
-    }
+    // function initGA() {
+    //     (function (i, s, o, g, r, a, m) {
+    //         i.GoogleAnalyticsObject = r;
+    //         i[r] = i[r] || function () {
+    //             (i[r].q = i[r].q || []).push(arguments);
+    //         }, i[r].l = 1 * new Date();
+    //         a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+    //         a.async = 1;
+    //         a.src = g;
+    //         m.parentNode.insertBefore(a, m);
+    //     })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+    //     ga('create', 'UA-69824169-2', 'auto');
+    //     ga('set', 'checkProtocolTask', null);
+    //     ga('set', 'anonymizeIp', true);
+    //     ga('send', 'pageview');
+    // }
 
     function isSiteEnabled(details) {
         const enabledSite = matchUrlDomain(enabledSites, details.url);
